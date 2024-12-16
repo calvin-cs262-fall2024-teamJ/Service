d651a974e9aa3cd37f014fcfa3581441a4a38174
ChaseVelloney <crv37@calvin>
2024-12-15 21:54:45 -0500

    reset

--- dungeonmasterService.js
+++ dungeonmasterService.js
@@ -1,266 +1,69 @@
+
+
+// Set up the database connection.
+
 const pgp = require('pg-promise')();
+
 const db = pgp({
   host: process.env.DB_SERVER,
   port: process.env.DB_PORT,
   database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  ssl: { rejectUnauthorized: false },
+  ssl: {rejectUnauthorized: false},
 });
+
+// Configure the server and its routes.
+
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 8080;
 const router = express.Router();
 router.use(express.json());
 
-// Logging environment variables
+//logging
 console.log('DB_SERVER:', process.env.DB_SERVER);
 console.log('DB_PORT:', process.env.DB_PORT);
 console.log('DB_DATABASE:', process.env.DB_DATABASE);
 console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 
-// DungeonMaster functions
+//dms
+router.get('/', readHelloMessage);
 router.get('/dungeonmasters', readDMs);
 router.get('/dungeonmasters/:id', readDM);
 router.put('/dungeonmasters/:id', updateDM);
 router.post('/dungeonmasters', createDM);
 
-function readDMs(req, res, next) {
-  db.any('SELECT * FROM DungeonMaster;')
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.error('Error fetching DungeonMasters:', err);
-      next(err);
-    });
-}
-
-function readDM(req, res, next) {
-  db.oneOrNone('SELECT * FROM DungeonMaster WHERE ID=${id};', req.params)
-    .then((data) => {
-      returnDataOr404(res, data);
-    })
-    .catch((err) => {
-      console.error('Error fetching DungeonMaster:', err);
-      next(err);
-    });
-}
-
-function updateDM(req, res, next) {
-  db.oneOrNone('UPDATE DungeonMaster SET nickname=${body.nickname}, loginid=${body.loginid}, password=${body.password} WHERE ID=${params.id} RETURNING ID;', req)
-    .then((data) => {
-      returnDataOr404(res, data);
-    })
-    .catch((err) => {
-      console.error('Error updating DungeonMaster:', err);
-      next(err);
-    });
-}
-
-function createDM(req, res, next) {
-  db.one('INSERT INTO DungeonMaster(nickname, loginid, password) VALUES (${nickname}, ${loginid}, ${password}) RETURNING ID;', req.body)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.error('Error creating DungeonMaster:', err);
-      next(err);
-    });
-}
-
-// Map functions
+//maps
 router.get('/maps', readMaps);
-router.get('/maps/image/:id', readMapImage);
+router.get('/maps/image/:id', readMapImage)
+// router.put('/maps/:id', updateMap);
 router.post('/maps', createMap);
 router.delete('/maps/:id', deleteMap);
 
-function readMaps(req, res, next) {
-  db.any('SELECT * FROM Map;')
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.error('Error fetching maps:', err);
-      next(err);
-    });
-}
-
-function readMapImage(req, res, next) {
-  db.one('SELECT MapImage FROM Map WHERE ID=${id};', req.params)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.error('Error fetching map image:', err);
-      next(err);
-    });
-}
-
-function createMap(req, res, next) {
-  db.one('INSERT INTO Map(DungeonMasterID, MapImage, MapName, supermapID) VALUES (${DungeonMasterID}, ${MapImage}, ${MapName}, ${supermapID}) RETURNING ID;', req.body)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.error('Error creating map:', err);
-      next(err);
-    });
-}
-
-function deleteMap(req, res, next) {
-  db.oneOrNone('DELETE FROM Map WHERE ID=${id} RETURNING ID;', req.params)
-    .then((data) => {
-      returnDataOr404(res, data);
-    })
-    .catch((err) => {
-      console.error('Error deleting map:', err);
-      next(err);
-    });
-}
-
-// Note functions
-router.get('/notes/:id', readNotes);
-router.get('/notes/map/:id', readMapNotes);
-router.get('/note/:id', readNote);
-router.post('/notes', createNote);
+// //note
+router.get('/notes', readNotes);
+router.get('/notes/:id', readNote);
 router.put('/notes/:id', updateNote);
+router.post('/notes', createNote);
 router.delete('/notes/:id', deleteNote);
+router.get('/notes/map/:id', readMapNotes)
 
-function readNotes(req, res, next) {
-  const worldMapID = req.params.id;
-  console.log(`Fetching notes for WorldMapID: ${worldMapID}`);
-
-  db.any('SELECT * FROM Note WHERE WorldMapID=${id};', { id: worldMapID })
-    .then((data) => {
-      console.log('Notes fetched:', data);
-      res.send(data);
-    })
-    .catch((err) => {
-      console.error(`Error fetching notes for WorldMapID: ${worldMapID}`, err);
-      res.status(500).send('Error fetching notes');
-    });
-}
-
-function readMapNotes(req, res, next) {
-  db.any('SELECT Title, Content FROM Note WHERE WorldMapID=${id};', req.params)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.error('Error fetching map notes:', err);
-      next(err);
-    });
-}
-
-function readNote(req, res, next) {
-  db.one('SELECT ID, Content FROM Note WHERE ID=${id};', req.params)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.error('Error fetching note:', err);
-      next(err);
-    });
-}
-
-function createNote(req, res, next) {
-  db.one('INSERT INTO Note(WorldMapID, Title, Content) VALUES (${WorldMapID}, ${Title}, ${Content}) RETURNING ID;', req.body)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.error('Error creating note:', err);
-      next(err);
-    });
-}
-
-function deleteNote(req, res, next) {
-  db.oneOrNone('DELETE FROM Note WHERE ID=${id} RETURNING ID;', req.params)
-    .then((data) => {
-      returnDataOr404(res, data);
-    })
-    .catch((err) => {
-      console.error('Error deleting note:', err);
-      next(err);
-    });
-}
-
-function updateNote(req, res, next) {
-  db.oneOrNone('UPDATE Note SET Title=${body.Title}, Content=${body.Content} WHERE ID=${params.id} RETURNING ID;', req)
-    .then((data) => {
-      returnDataOr404(res, data);
-    })
-    .catch((err) => {
-      console.error('Error updating note:', err);
-      next(err);
-    });
-}
-
-// Pin functions
-router.get('/pins/:id', readPins);
+// //pin
+router.get('/pins/:id', readPins);//may need to change
+// router.get('/pins/:id', readPin);
 router.put('/pins/:id', updatePin);
 router.post('/pins', createPin);
 router.delete('/pins/:id', deletePin);
+//pin image
 router.post('/pins/images', createPinImage);
 
-function readPins(req, res, next) {
-  db.any('SELECT Pin.ID, Pin.NoteID, Pin.x, Pin.y, Pin.iconID, Pin.color FROM Pin JOIN Note ON Pin.NoteID = Note.ID WHERE WorldMapID=${id};', req.params)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.error('Error reading pins:', err);
-      next(err);
-    });
-}
+app.use(router);
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
-function updatePin(req, res, next) {
-  db.oneOrNone('UPDATE Pin SET NoteID=${body.NoteID}, x=${body.x}, y=${body.y}, iconID=${body.iconID}, color=${body.color} WHERE ID=${params.id} RETURNING ID;', req)
-    .then((data) => {
-      returnDataOr404(res, data);
-    })
-    .catch((err) => {
-      console.error('Error updating pin:', err);
-      next(err);
-    });
-}
+// Implement the CRUD operations.
 
-function createPin(req, res, next) {
-  db.one('INSERT INTO Pin(NoteID, x, y, iconID, color) VALUES (${NoteID}, ${x}, ${y}, ${iconID}, ${color}) RETURNING ID;', req.body)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.error('Error creating pin:', err);
-      next(err);
-    });
-}
-
-function deletePin(req, res, next) {
-  db.oneOrNone('DELETE FROM Pin WHERE ID=${id} RETURNING ID;', req.params)
-    .then((data) => {
-      returnDataOr404(res, data);
-    })
-    .catch((err) => {
-      console.error('Error deleting pin:', err);
-      next(err);
-    });
-}
-
-function createPinImage(req, res, next) {
-  db.one('INSERT INTO PinImage(icon) VALUES (${icon}) RETURNING ID;', req.body)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.error('Error creating pin image:', err);
-      next(err);
-    });
-}
-
-// Common utility function
 function returnDataOr404(res, data) {
   if (data == null) {
     res.sendStatus(404);
@@ -269,6 +72,247 @@ function returnDataOr404(res, data) {
   }
 }
 
-// Ensure this line is at the very bottom of the script
-app.use(router);
-app.listen(port, () => console.log(`Listening on port ${port}`));
+function readHelloMessage(req, res) {
+  res.send('Hello, JourneySmith WebService!');
+}
+
+//dungeon master functions
+function readDMs(req, res, next) {
+  db.any('SELECT * FROM DungeonMaster;')//maybe db.many
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function readDM(req, res, next) {
+  db.oneOrNone('SELECT * FROM DungeonMaster WHERE id=${id};', req.params)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function updateDM(req, res, next) {
+  db.oneOrNone('UPDATE DungeonMaster SET nickname=${body.nickname}, loginid=${body.loginid}, password=${body.password} WHERE id=${params.id} RETURNING id;', req)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function createDM(req, res, next) {
+  db.one('INSERT INTO DungeonMaster(nickname, loginid, password) VALUES (${nickname}, ${loginid}, ${password}) RETURNING id;', req.body)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+// function deleteDM(req, res, next) {
+//   db.oneOrNone('DELETE FROM DungeonMaster WHERE id=${id} RETURNING id;', req.params)
+//     .then((data) => {
+//       returnDataOr404(res, data);
+//     })
+//     .catch((err) => {
+//       next(err);
+//     });
+// }
+
+//Map functions
+function readMaps(req, res, next){
+  db.any('SELECT * FROM Map;')
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function readMapImage(req, res, next){
+  db.one('SELECT MapImage FROM Map WHERE DungeonMasterID=${id};', req.params)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function createMap(req, res, next) {
+  db.one('INSERT INTO Map(DungeonMasterID, MapImage, MapName) VALUES (${DungeonMaster}, ${MapImage}, ${MapName}) RETURNING id;', req.body)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function deleteMap(req, res, next) {
+  db.oneOrNone('DELETE FROM Map WHERE id=${id} RETURNING id;', req.params)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+//Note functions
+
+//reads all SQL Note data from given dungeonMaster
+function readNotes(req, res, next){
+  console.log('Accessing /notes');
+  db.any('SELECT * FROM Note WHERE WorldMapID=${id};', req.params)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+//reads Notes content from the same given map id
+function readMapNotes(req, res, next){
+  db.any('SELECT Title, Content FROM Note WHERE WorldMapID=${id};', req.params)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+//reads one specific note based off given note id
+function readNote(req, res, next){
+  console.log('Accessing /note/:id');
+  db.one('SELECT ID, Content FROM Note WHERE id=${id};',req.params)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function createNote(req, res, next) {
+  console.log('Accessing /notes creat Note');
+  db.one('INSERT INTO Note(WorldMapID, Title, Content) VALUES (${WorldMapID}, ${MapImage}, ${MapName}) RETURNING id;', req.body)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function deleteNote(req, res, next) {
+  db.oneOrNone('DELETE FROM Note WHERE id=${id} RETURNING id;', req.params)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function updateNote(req, res, next) {
+  db.oneOrNone('UPDATE Note SET Title=${body.Title}, Content=${body.Content} WHERE id=${params.id} RETURNING id;', req)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+//Pin function
+function readPins(req, res, next) {
+  db.any('SELECT Pin.ID, Pin.NoteID, Pin.x, Pin.y, Pin.iconID FROM Pin, Note WHERE WorldMapID=${id} AND NoteID=Note.ID;', req)//maybe db.many
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function updatePin(req, res, next) {
+  db.oneOrNone('UPDATE Pin SET NoteID=${body.NoteID}, x=${body.x}, y=${body.y}, iconID=${body.iconID} WHERE id=${params.id} RETURNING id;', req)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function createPin(req, res, next) {
+  db.one('INSERT INTO Pin(NoteID, x, y, iconID) VALUES (${NoteID}, ${x}, ${y}, ${iconID}) RETURNING id;', req.body)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function deletePin(req, res, next) {
+  db.oneOrNone('DELETE FROM Pin WHERE id=${id} RETURNING id;', req.params)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function createPinImage(req, res, next) {
+  db.one('INSERT INTO PinImage(icon) VALUES (${icon}) RETURNING id;', req.body)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+app.get('/notes', async (req, res, next) => {
+  try {
+    console.log('Fetching notes...'); // Debug log
+    const notes = await db.any('SELECT * FROM Note WHERE WorldMapID=${id};', req.params);
+    console.log('Notes fetched:', notes); // Debug log
+    res.send(notes);
+  } catch (err) {
+    console.error('Error fetching notes:', err); // Debug log
+    next(err);
+  }
+});
+
+router.get('/notes/:id', async (req, res, next) => {
+  try {
+    console.log('Fetching notes for WorldMapID:', req.params.id); // Debug log
+    const notes = await db.any('SELECT * FROM Note WHERE WorldMapID=${id};', req.params);
+    console.log('Database query executed successfully'); // Debug log
+    console.log('Notes fetched:', notes); // Debug log
+    if (notes.length === 0) {
+      console.log('No notes found for the given WorldMapID'); // Debug log
+    }
+    res.send(notes);
+  } catch (err) {
+    console.error('Error fetching notes for WorldMapID:', req.params.id, err); // Debug log
+    res.status(500).send('Error fetching notes');
+  }
+});
+
+
--- sql/database.sql
+++ sql/database.sql
@@ -1,8 +1,9 @@
--- Drop tables if they exist
+-- Drop existing tables if they exist
 DROP TABLE IF EXISTS Pin;
 DROP TABLE IF EXISTS Note;
 DROP TABLE IF EXISTS Map;
 DROP TABLE IF EXISTS DungeonMaster;
+DROP TABLE IF EXISTS PinImage;
 
 -- Create DungeonMaster table
 CREATE TABLE DungeonMaster (
@@ -12,21 +13,21 @@ CREATE TABLE DungeonMaster (
     password VARCHAR(30) NOT NULL
 );
 
--- Create Note table
-CREATE TABLE Note (
-    ID SERIAL PRIMARY KEY,
-    WorldMapID INTEGER REFERENCES Map(ID) ON DELETE SET NULL,
-    Title VARCHAR(50),
-    Content TEXT
-);
-
 -- Create Map table
 CREATE TABLE Map (
     ID SERIAL PRIMARY KEY,
     DungeonMasterID INTEGER REFERENCES DungeonMaster(ID) ON DELETE CASCADE,
     MapImage BYTEA,
-    MapName VARCHAR(50),
+    MapName VARCHAR(50) NOT NULL,
     supermapID INTEGER REFERENCES Map(ID) ON DELETE SET NULL
+);
+
+-- Create Note table
+CREATE TABLE Note (
+    ID SERIAL PRIMARY KEY,
+    WorldMapID INTEGER REFERENCES Map(ID) ON DELETE CASCADE,
+    Title VARCHAR(50),
+    Content TEXT
 );
 
 -- Create Pin table
@@ -39,18 +40,27 @@ CREATE TABLE Pin (
     color VARCHAR(20)
 );
 
+-- Create PinImage table
+CREATE TABLE PinImage (
+    ID SERIAL PRIMARY KEY,
+    icon BYTEA
+);
+
 -- Grant SELECT on tables to public
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO PUBLIC;
 
--- Insert example data
+-- Insert example data into DungeonMaster table
 INSERT INTO DungeonMaster (nickname, loginid, password) VALUES ('dm1', 1, 'password1');
 INSERT INTO DungeonMaster (nickname, loginid, password) VALUES ('dm2', 2, 'password2');
 
+-- Insert example data into Map table
 INSERT INTO Map (DungeonMasterID, MapImage, MapName) VALUES (1, decode('42696e6172792044617461', 'hex'), 'Rivendel');
 INSERT INTO Map (DungeonMasterID, MapImage, MapName) VALUES (2, decode('42696e6172792044617461', 'hex'), 'Mordor');
 
+-- Insert example data into Note table
 INSERT INTO Note (WorldMapID, Title, Content) VALUES (1, 'Note1', 'Content for Note 1');
 INSERT INTO Note (WorldMapID, Title, Content) VALUES (2, 'Note2', 'Content for Note 2');
 
+-- Insert example data into Pin table
 INSERT INTO Pin (NoteID, x, y, iconID, color) VALUES (1, 3, 4, 1, 'blue');
 INSERT INTO Pin (NoteID, x, y, iconID, color) VALUES (2, 6, 7, 2, 'red');
