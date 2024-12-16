ce06e68768abcd6761a9cbd6755d395aa99d3778
ChaseVelloney <112497022+ChaseVelloney@users.noreply.github.com>
2024-12-15 22:55:19 -0500

    Update dungeonmasterService.js

--- dungeonmasterService.js
+++ dungeonmasterService.js
@@ -6,7 +6,7 @@ const pgp = require('pg-promise')();
 
 const db = pgp({
   host: process.env.DB_SERVER,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 5432,
   database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
