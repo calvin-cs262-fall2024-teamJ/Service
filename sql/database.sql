e2b4226b5f0b3c6def5963a9beac63061d256c70
ChaseVelloney <112497022+ChaseVelloney@users.noreply.github.com>
2024-12-16 18:31:21 -0500

    Update database.sql

--- sql/database.sql
+++ sql/database.sql
@@ -1,60 +1,73 @@
-
---DROP TABLE IF EXISTS NULL;
+-- Drop existing tables if they exist
 DROP TABLE IF EXISTS Pin;
 DROP TABLE IF EXISTS Note;
 DROP TABLE IF EXISTS Map;
 DROP TABLE IF EXISTS DungeonMaster;
+DROP TABLE IF EXISTS PinImage;
 
-CREATE TABLE DungeonMaster(--main user
-    ID integer Primary Key,
-    nickname char(20) NOT NULL,
-    loginid integer NOT NULL,
-    password char(30) NOT NULL -- hopefuly will be encrypted
+-- Create DungeonMaster table
+CREATE TABLE DungeonMaster (
+    ID SERIAL PRIMARY KEY,
+    nickname VARCHAR(20) NOT NULL,
+    loginid INTEGER NOT NULL,
+    password VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE Map(
-    ID integer Primary Key,
-    DungeonMasterID integer REFERENCES DungeonMaster(ID) NOT NULL,
-    MapImage OID,--MAY NEED to be changed to hold file
-    MapName char(20) NOT NULL,
-    supermapID integer REFERENCES Map(ID) -- should be null for first map
+-- Create Map table
+CREATE TABLE Map (
+    ID SERIAL PRIMARY KEY,
+    DungeonMasterID INTEGER REFERENCES DungeonMaster(ID) NOT NULL,
+    MapImage BYTEA,
+    MapName VARCHAR(50) NOT NULL,
+    supermapID INTEGER REFERENCES Map(ID) ON DELETE SET NULL
 );
 
-CREATE TABLE Note(
-    ID integer Primary Key,
-    WorldMapID integer REFERENCES Map(ID),
-    Title char(20),
-    Content text
+-- Create Note table
+CREATE TABLE Note (
+    ID SERIAL PRIMARY KEY,
+    WorldMapID INTEGER REFERENCES Map(ID),
+    Title VARCHAR(50),
+    Content TEXT
 );
 
-CREATE TABLE Pin(
-    ID integer Primary Key,
-    NoteID integer REFERENCES Note(ID),--this needs to change probably switched
-    x integer NOT NULL,
-    y integer NOT NULL,
-    --color char(20),
-    iconID integer REFERENCES PinImage(ID) -- also needs to change for image
+-- Create PinImage table
+CREATE TABLE PinImage (
+    ID SERIAL PRIMARY KEY,
+    icon BYTEA
 );
 
-CREATE TABLE PinImage(
-    ID integer Primary Key,
-    icon OID
-)
-GRANT SELECT ON DungeonMaster TO PUBLIC;
-GRANT SELECT ON Map TO PUBLIC;
-GRANT SELECT ON Note TO PUBLIC;
-GRANT SELECT ON Pin TO PUBLIC;
+-- Create Pin table
+CREATE TABLE Pin (
+    ID SERIAL PRIMARY KEY,
+    NoteID INTEGER REFERENCES Note(ID),
+    x INTEGER NOT NULL,
+    y INTEGER NOT NULL,
+    iconID INTEGER REFERENCES PinImage(ID),
+    color VARCHAR(20)
+);
 
+-- Insert example data into DungeonMaster table
+INSERT INTO DungeonMaster (nickname, loginid, password) VALUES ('THE LICH', 1, 'hello world');
+INSERT INTO DungeonMaster (nickname, loginid, password) VALUES ('NERUL', 2, 'this password');
 
--- --TESTING
-INSERT INTO DungeonMaster VALUES (1,'THE LICH',1,'hello world');
-INSERT INTO DungeonMaster VALUES (2,'NERUL',2,'this password');
+-- Insert example data into Map table
+INSERT INTO Map (DungeonMasterID, MapImage, MapName, supermapID) VALUES (1, decode('42696e6172792044617461', 'hex'), 'Rivendel', NULL);
+INSERT INTO Map (DungeonMasterID, MapImage, MapName, supermapID) VALUES (2, decode('42696e6172792044617461', 'hex'), 'Mordor', NULL);
 
-INSERT INTO Map(ID, DungeonMasterID, MapImage, MapName,supermapID) VALUES (1,1,'ok','Rivendel',NULL);
-INSERT INTO Map VALUES (1,1,'ok','Rivendel',NULL);
+-- Insert example data into Note table
+INSERT INTO Note (WorldMapID, Title, Content) VALUES (1, 'Note1', 'Content for Note 1');
+INSERT INTO Note (WorldMapID, Title, Content) VALUES (2, 'Note2', 'Content for Note 2');
 
-INSERT INTO Note VALUES (1,1,'silk song will come out soon, I can feel it');
-INSERT INTO Pin VALUES (1,1,3,4,'blue','pentagon');
+-- Insert example data into PinImage table
+INSERT INTO PinImage (icon) VALUES (decode('42696e6172792044617461', 'hex'));  -- Example binary data for icons
+INSERT INTO PinImage (icon) VALUES (decode('42696e6172792044617461', 'hex'));  -- Example binary data for icons
+
+-- Insert example data into Pin table
+INSERT INTO Pin (NoteID, x, y, iconID, color) VALUES (1, 3, 4, 1, 'blue');
+INSERT INTO Pin (NoteID, x, y, iconID, color) VALUES (2, 6, 7, 2, 'red');
+
+-- Grant SELECT on tables to public
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO PUBLIC;
 
 -- SELECT * FROM DungeonMaster;
 -- SELECT * FROM Note;
